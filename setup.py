d37366eceaaf24b4598d01034309e6f7e4396bba
kevin72500 <kevin72500@qq.com>
2021-12-18 14:01:04 +0800

    add dependency

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/kevin72500/khandytool",
     packages=setuptools.find_packages(),
-    install_requires=['faker==8.12.1','jmespath==0.9.5','loguru==0.4.1','pymysql==1.0.2','xmindparser==1.0.9','openpyxl==3.0.9','pytest==6.2.5','fabric==2.6.0','pywebio==1.4.0','requests==2.26.0','jinja2==3.0.2','swaggerjmx==1.0.9'],
+    install_requires=['faker==8.12.1','jmespath==0.9.5','loguru==0.4.1','pymysql==1.0.2','xmindparser==1.0.9','openpyxl==3.0.9','pytest==6.2.5','fabric==2.6.0','pywebio==1.4.0','requests==2.26.0','jinja2==3.0.2','swaggerjmx==1.0.9','kafka-python==2.0.2'],
     entry_points={
         'console_scripts': [
             'khandytool=khandytool:core'
