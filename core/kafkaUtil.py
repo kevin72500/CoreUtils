315d1ceffb309c68794efd77c48012b9ef20c8c4
kevin72500 <kevin72500@qq.com>
2021-12-18 13:52:16 +0800

    fixbug

--- core/kafkaUtil.py
+++ core/kafkaUtil.py
@@ -6,6 +6,7 @@ from kafka.client_async import KafkaClient
 import jmespath
 import threading
 from loguru import logger
+import re
 
 logger.add('kafkaUtil_log.txt',encoding='utf-8')
 
@@ -20,42 +21,78 @@ class kafkaOper(object):
     def getProducer(self,clientid="kafkaOper"):
         self.kafkaConnection=KafkaProducer(bootstrap_servers=self.bootstrapserver,client_id=clientid)
         return self
-    def doFileterFromComsumer(self,jmeshPath="",regMatchString="",matchStr="",keepListen=False,store=False):
-        print('Start filter....')
+
+    def jsonFilter(self,allStr,pattern,key):
+        try:
+        # print(flag)
+            allStr = json.loads(allStr.value.decode())
+            flag=jmespath.search(pattern,allStr)
+            if flag==key:
+                logger.warning(f'Topic: {self.topic} 在时间：{datetime.datetime.now()}->结果匹配到: {key}了')
+                logger.info(f'Total String is: {allStr}')
+            # else:
+            #     logger.info(f'{allStr}')
+        except Exception as e:
+            logger.error('json 解析失败')
+            logger.error(f'{allStr}')
+
+    def regxFilter(self,allStr,pattern, key):
+        try:
+            allStr = allStr.value.decode()
+            flag=re.compile(pattern)
+            res=flag.findall(allStr)
+            # print(res)
+            if key in "".join(res):
+                logger.warning(f'Topic: {self.topic} 在时间：{datetime.datetime.now()}->结果匹配到: {res}了')
+                logger.info(f'Total String is: {allStr}')
+            # else:
+            #     logger.info(f'{allStr}')
+        except Exception as e:
+            logger.error('regx 解析失败')
+            logger.error(f'{allStr}') 
+
+
+    def doFileterFromComsumer(self,flag="",pattern="",matchStr="",keepListen=False,store=False):
+        '''
+            flag = "json"
+            or
+            flag ="regx"
+        '''
+        logger.info('Start filter....')
         resStore=[]
         for one in self.kafkaConnection:
-            # print(one.value.decode())
-            businessData = json.loads(one.value.decode())
-            if regMatchString!="" and jmeshPath!="":
-                logger.error("can't using both jsonPath and regx filter in the same time")
-                return False
-            try:
-                # print(flag)
-                if jmeshPath!="":
-                    flag=jmespath.search(jmeshPath,businessData)
-                    if flag==matchStr:
-                        logger.warning(f'Topic: {self.topic} 在时间：{datetime.datetime.now()}->结果匹配到: {matchStr}了')
-                        logger.info(f'{one.value.decode()}')
-                else:
-                    logger.info(f'{one.value.decode()}')
-            except Exception as e:
-                logger.error('json 解析失败')
-                logger.error(f'{one.value.decode()}')
+            # print(one)
+            # print(flag)
+            if flag=="json":
+                # logger.info(f"json {one}")
+                self.jsonFilter(allStr=one,pattern=pattern,key=matchStr)
+            elif flag=="regx":
+                # logger.info(f"regx {one}")
+                self.regxFilter(allStr=one,pattern=pattern,key=matchStr)
+            elif flag=="":
+                logger.warning(f'Topic: {self.topic} 在时间：{datetime.datetime.now()}->未加入匹配的消息是: {one}了')
+                
+
+                
     def doSendFromProducer(self,message,partition=None,key=None):
         self.kafkaConnection.send(self.topic,value=bytes(message,encoding='utf-8'),partition=partition,key=key)
 
 
-def general_monitor(topic="",serverAndPort="localhost:9092",pattern="",key=""):
-    kafkaOper(topic,serverAndPort).getConsumer().doFileterFromComsumer(pattern,key)
+def general_sender(topic="",serverAndPort="localhost:9092",message=""):
+    kafkaOper(topic,serverAndPort).getProducer().doSendFromProducer(message)
 
-def multi_topic_listener(topicList=[],serverAndPortList=[],patternList=[],keyList=[]):
+def general_listener(topic="",serverAndPort="localhost:9092",flag="",pattern="",key=""):
+    kafkaOper(topic,serverAndPort).getConsumer().doFileterFromComsumer(flag,pattern,key)
+
+def multi_topic_listener(topicList=[],serverAndPortList=[],flagList=[],patternList=[],keyList=[]):
     threadList=[]
-    for one in zip(topicList,serverAndPortList,patternList,keyList):
-        (a,b,c,d)=one
+    for one in zip(topicList,serverAndPortList,flagList,patternList,keyList):
+        (a,b,c,d,e)=one
         # print(f"{a},{b},{c},{d}")
-        threadList.append(threading.Thread(target=general_monitor,args=(a,b,c,d)))
+        temp=threading.Thread(target=general_listener,args=(a,b,c,d,e))
+        temp.start()
+        threadList.append(temp)
     for one in threadList:
-        one.start()
         one.join()
 
 
@@ -64,4 +101,25 @@ if __name__ == '__main__':
     # ,patternList=["json.data.id","json.data.id","json.data.id"],keyList=["assertingData1","assertingData2","assertingData3"])
 
 
-    multi_topic_listener(topicList=['topic',],serverAndPortList=["ip:port"],patternList=["json.data.id"],keyList=["assertingData"])
+    # multi_topic_listener(topicList=['testTopic'],
+    # serverAndPortList=["192.168.125.145:9092"],
+    # flagList=["regx"],
+    # partternList=["abb(.*)bba"],
+    # keyList=["555"])
+
+
+    multi_topic_listener(topicList=['iotHub','device_default_prop','device_default_state'],
+    serverAndPortList=["192.168.125.145:9092","192.168.125.145:9092","192.168.125.145:9092"],
+    flagList=["json","json","json"],
+    patternList=["payload.virDevUid","payload.virDevUid","payload.deviceId"],
+    keyList=["914959009603264531","914959009603264531","914959009603264536"])
+
+    
+    # general_listener(topic="testTopic",serverAndPort="192.168.125.145:9092",flag="json",pattern="abc.bcd",key=555)
+
+
+    # general_listener(topic="testTopic",serverAndPort="192.168.125.145:9092",flag="regx",pattern=r"abb(.*)bba",key="555")
+    # general_sender(topic="testTopic",serverAndPort="192.168.125.145:9092",message='{"abc":{"bcd":"555"}}')
+
+    # k=kafkaOper(topic='ab',bootstrapserver='ab')
+    # k.regxFilter("abb555bbc","abb(.*)bbc","555")
