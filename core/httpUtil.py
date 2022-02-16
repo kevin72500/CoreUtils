1a599c11c163ffb68a52b6a76bc00be77caa14e0
kevin72500 <kevin72500@qq.com>
2022-02-16 22:25:52 +0800

    add correlation common function in http

--- core/httpUtil.py
+++ core/httpUtil.py
@@ -1,6 +1,8 @@
-import requests
+import requests,os
 from loguru import logger
 from core.utils import jsonFilter,regxFilter,containFilter,findBy
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(verbose=True)
 class HttpOper:
     def __init__(self):
         """session管理器, 后续引入登录或者token处理"""
@@ -9,11 +11,12 @@ class HttpOper:
         self.exportParam={}
 
     def call(self, method, url, params=None, data=None, json=None, headers=None, **kwargs):
-        self.res=self.session.request(method,url, params=params, data=data, json=json, headers=headers,**kwargs)
+        logger.info(f'url : {url}')
+        self.res=self.session.request(method, url, params=params, data=data, json=json, headers=headers,**kwargs)
         return self
 
     def resCheck(self,flag="",pattern="",key=""):
-        logger.info(f'oriStr is: {self.res.text}')
+        logger.info(f'oriStr : {self.res.text}')
         if flag=='json':
             if not jsonFilter(self.res.text, flag, pattern, key)[0]:
                 return False
@@ -27,29 +30,38 @@ class HttpOper:
                 return False
             return True
 
-    def resCorrelate(self,paramName,flag="",pattern=""):
+    def setExportParam(self,paramName,flag="",pattern=""):
         logger.info(f'oriStr is: {self.res.text}')
         temp=findBy(self.res.text,flag=flag,pattern=pattern)
         if temp!=False:
             self.exportParam[paramName]=temp
         else:
-            logger.warning(f'can not get param by pattern {pattern}')
+            logger.warning(f'can not set exportParm by pattern {pattern}')
+        return self
 
     def getRes(self):
         return self.res
-        
+    
+    def getExportParam(self,key):
+        return self.exportParam[key]
+
     def close_session(self):
         """关闭session"""
         self.session.close()
 
 if __name__ == '__main__':
+    host=os.getenv("test_ip1")
 
-    url = 'http://192.168.xxx.xxx:9499/device/v1/api/devices/926499854176743469'
+    url = 'http://'+host+':9499/device/v1/api/devices/926499854176743469'
     header={"content-type":"application/json;charset=UTF-8"}
     req = HttpOper()
-    # res = req.call("get", url, headers=header).resCheck(flag="json", pattern="data.id", key="926499854176743469")
-    # print(res)
+
+    res = req.call("get", url, headers=header).resCheck(flag="json", pattern="data.id", key="926499854176743469")
+    print(res)
     res = req.call("get", url, headers=header).resCheck(flag="regx", pattern="classifyName\":\"(.*)\"", key="水冷主机")
     print(res)
-    # res = req.call("get", url, headers=header).resCheck(flag="contain", key="926499854176743469")
-    # print(res)
+    res = req.call("get", url, headers=header).resCheck(flag="contain", key="926499854176743469")
+    print(res)
+
+    res = req.call("get", url, headers=header).setExportParam('p_name','json','data.productName')
+    print(res.getExportParam('p_name'))
