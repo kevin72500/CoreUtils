7ddbb9b5b2acb41052dab65100e6c7d3a231bcd7
kevin72500 <kevin72500@qq.com>
2022-02-18 21:25:56 +0800

    add har2jmeter

---
+++ core/jmeterUtil/jmeterUtil.py
@@ -0,0 +1,22 @@
+from swaggerjmx.convert import conversion
+from swaggerjmx.settings import Settings as ST
+import traceback
+
+
+
+def swagger2jmeter(url):
+
+    #  swagger_url
+    try:
+        ST.swagger_url = url
+        #  report_path
+        ST.report_path = 'jmx'
+        # 开始转换
+        conversion()
+        # print(os.path.abspath(os.path.dirname(__file__))+os.sep+'jmx'+os.sep)
+        upperDir=os.path.abspath(os.path.dirname(__file__))+os.sep+'jmx'+os.sep
+        for x,y,z in os.walk(os.path.abspath(os.path.dirname(__file__))+os.sep+'jmx'+os.sep):
+            return upperDir+"".join(z)
+    except Exception as e:
+        print(traceback.format_exc())
+        return "switch to jmeter script error"
